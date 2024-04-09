c2d26cd9edf0e5f8bde080974eab6223dc92f3c9
khushi <khushi.bedar@tmdc.io>
2024-04-09 16:24:49 +0530

    added models

--- models/stag/transaction_src_view.sql
+++ models/stag/transaction_src_view.sql
@@ -1,3 +1,3 @@
 {{ config(materialized='view') }}
 
-select count(transaction_id) as transaction_counts ,transaction_status from {{source('source_v1', 'transaction_source')}} group by transaction_status
+select count(transaction_id) as transaction_counts ,transaction_status from {{source('source_v1', 'transaction_sources')}} group by transaction_status
